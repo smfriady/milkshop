998dfd44b262a4029d38cf98844bd79f6498d7e7
Sony MelliFriady <sonymellifriady@gmail.com>
2026-01-20 07:14:08 +0700

    cart in product screen

--- frontend/src/contexts/CartContext.jsx
+++ frontend/src/contexts/CartContext.jsx
@@ -11,7 +11,31 @@ const CartProvider = ({ children }) => {
     CartStorage.save(cart);
   }, [cart]);
 
-  let value = { cart };
+  // add to cart
+  const addToCart = (product) => {
+    setCart((prevProduct) => {
+      let isExistingProduct = prevProduct.find(
+        (item) => item._id === product._id,
+      );
+
+      if (isExistingProduct) {
+        return prevProduct;
+      }
+      return [...prevProduct, { ...product, qty: 1 }];
+    });
+  };
+
+  // product existing
+  const existingProduct = (product) => {
+    let isExistingProduct = cart.find((item) => item._id === product._id);
+    if (isExistingProduct) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  let value = { cart, addToCart, existingProduct };
 
   return <CartContext value={value}>{children}</CartContext>;
 };
